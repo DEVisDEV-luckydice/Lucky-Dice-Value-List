74b9534ff29ade0febe039dc696dbfbd0a312f6d
DEVisDEV-luckydice <thaolam120196@gmail.com>
2026-05-22 18:17:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,32 +2,145 @@ const items = [
 
   {
     name: "Golden Dice",
-    value: 500,
-    rarity: "Legendary"
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
   },
 
   {
     name: "Forest Lantern",
-    value: 120,
-    rarity: "Rare"
-  }
+    value: "120",
+
+    image:
+    "https://images.unsplash.com/photo-1512453979798-5ea266f8880c?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
+
+  {
+    name: "Golden Dice",
+    value: "500",
+
+    image:
+    "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=1200&auto=format&fit=crop"
+  },
 
 ]
 
-const container = document.getElementById("itemContainer")
+const itemContainer =
+document.getElementById("itemContainer")
 
 items.forEach(item => {
 
-  container.innerHTML += `
-  
-    <div class="item">
-      <h2>${item.name}</h2>
+  itemContainer.innerHTML += `
 
-      <p>💰 Value: ${item.value}</p>
+    <div class="card">
 
-      <p>⭐ Rarity: ${item.rarity}</p>
+      <img src="${item.image}">
+
+      <div class="cardContent">
+
+        <div class="itemName">
+          ${item.name}
+        </div>
+
+        <div class="value">
+          💰 ${item.value}
+        </div>
+
+      </div>
+
     </div>
-  
+
   `
 
 })
+
+function showPage(pageId) {
+
+  const pages =
+  document.querySelectorAll(".page")
+
+  pages.forEach(page => {
+
+    page.classList.add("hidden")
+
+  })
+
+  document
+  .getElementById(pageId)
+  .classList
+  .remove("hidden")
+
+}
